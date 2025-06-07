d5b60217a8ed9a73ca9b9affbae199975831ae52
unknown <yutaagra@gmail.com>
2025-06-07 16:15:50 +0700

    Dev: Nyoba nyoba aja

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
 
 train = pd.read_csv("train.csv")
 train.head()
@@ -28,11 +29,11 @@ df = train.drop(columns=over)
 numeric_features = df.select_dtypes(include=['number']).columns
 
 # Mengaatasi Outliers 
-for feature in numeric_features: 
-    plt.figure(figsize=(10, 6))
-    sns.boxplot(x=df[feature])
-    plt.title(f'Box Plot of {feature}')
-    plt.show()
+# for feature in numeric_features: 
+#     plt.figure(figsize=(10, 6))
+#     sns.boxplot(x=df[feature])
+#     plt.title(f'Box Plot of {feature}')
+#     plt.show()
 
 # Identifikasi outliers dengan IQR
 
@@ -70,7 +71,48 @@ if not numeric_features.empty:
 else:
     print("Tidak ada fitur numerik yang tersedia untuk distandarisasi")
 
+# Identifikasi baris duplikat
+duplicates = df.duplicated()
 
+print("Baris duplikat:")
+print(df[duplicates])
+
+# Menghapus baris duplikat
+df = df.drop_duplicates()
+
+print("DataFrame setelah menghapus duplikat:")
+print(df)
+
+# Ordinal Encoding
+
+category_features = df.select_dtypes(include=['object']).columns
+df[category_features]
+
+# One Hot Encoding
+df_one_hot = pd.get_dummies(df, columns=category_features)
+df_one_hot
+
+# Inisialisasi LabelEncoder
+label_encoder = LabelEncoder()
+df_lencoder = pd.DataFrame(df)
+
+for col in category_features: 
+    df_lencoder[col] = label_encoder.fit_transform(df[col])
+
+df_lencoder
+
+df_lencoder.head()
+
+# Menghitung jumlah dan persentase missing
+missing_values = df_lencoder.isnull().sum()
+missing_percentage = (missing_values / len(df_lencoder)) * 100
+
+missing_data = pd.DataFrame({
+    'Missing Values': missing_values,
+    'Percentage': missing_percentage
+}).sort_values(by='Missing Values', ascending=False)
+
+missing_data[missing_data['Missing Values'] > 0]
 
 # sample = pd.read_csv("sample_submission.csv")
 # train.head()
